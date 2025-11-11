bbf6cc265cdc12fedce89878b0b5459c73b7c08b
unknown <rabab177hasan@gmail.com>
2025-11-11 13:05:44 +0300

    comment form done

---
+++ src/pages/CommentForm.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import Client from "../services/api"
+const CommentForm = ({ user }) => {
+  const initialState ={
+    description : ""
+  }
+  // initial state is empty
+  const [formValue, setFormValue] = useState(initialState)
+
+  const handleChange = (e) => {
+    setFormValue({ ...formValue, [e.target.name]: e.target.value })
+  }
+  const handleSubmit =async (e) => {
+    e.preventDefault()
+    const formData = { ...formValue, owner: user.id }
+    const response = await Client.post("/Game", formData)
+    setGame(response.data)
+    setFormValue(initialState)
+    navigate(`/Game/${response.data._id}`)
+  }
+  return user ? (
+    <div>
+    <form onSubmit={handleSubmit}>
+
+    <label htmlFor="description">Description</label>
+        <textarea
+          name="description"
+          value={formValue.description}
+          onChange={handleChange}
+          placeholder="Detailed job description..."
+          required
+        />
+
+        <button type="submit">Post</button>
+        </form>
+        </div>
+  ):null
+}
+export default CommentForm
